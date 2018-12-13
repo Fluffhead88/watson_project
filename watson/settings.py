5158a73a9886a5559605253ceaf5632d17bd0954
Fluffhead88 <thigpen.zach@gmail.com>
2018-12-12 22:33:33 -0500

    materialize template in place

--- watson/settings.py
+++ watson/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 AUTH_USER_MODEL = 'app.User'
 
